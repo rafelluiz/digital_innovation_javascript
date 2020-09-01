1fa3def9243c8ca9ab13ba780534e163d82b137f
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-09-01 17:32:09 -0300

    Teste dos filosofos

---
+++ Testes/01_Filosofos.js
@@ -0,0 +1,9 @@
+const filosofos = ['Socrates','Fulano','siclano'];
+
+const algunsFilosofo = filosofos.splice(1);
+
+console.log(algunsFilosofo)
+
+algunsFilosofo.push('Seneca')
+
+console.log(algunsFilosofo)
