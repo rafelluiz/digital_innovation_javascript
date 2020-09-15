90ea66b51570ab64113e18de17a817fcfd49289c
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-09-15 10:12:42 -0300

    Criando metodo movimentar e latir

--- orientacao_a_objetos/02_classe.js
+++ orientacao_a_objetos/02_classe.js
@@ -2,17 +2,26 @@
 
 class Animal {
   constructor(qtdePatas) {
-    this.qtdePatas = 4;
+    this.qtdePatas = 0;
   }
+
+  movimentar(){}
 }
 
 class Cachorro extends Animal{
   constructor(morde) {
     super();
+    this.qtdePatas = 4;
     this.morde = morde;
+  }
+
+  latir(){
+    console.log('Au! Au! Au!');
   }
 }
 
 const pug = new Cachorro(false);
+const pitbull = new Cachorro(true);
 
 console.log(pug);
+console.log(pitbull);
